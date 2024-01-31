9fd5a843f9b9004cb7c04881de820294ad38f42f
Adam Santorelli <148909356+Asanto32@users.noreply.github.com>
2024-01-31 14:51:29 -0500

    updatres to conform to standards, bug fixes/checks

--- src/actigrapy/ggir/metrics_calc.py
+++ src/actigrapy/ggir/metrics_calc.py
@@ -1,16 +1,53 @@
-"""Compute anglez and enmo
-Future use for calculating other metrics from  (x,y,z) and temp? 
-std(anglez), abs(diff(z))
-median/mean"""
+"""Compulation of function to calculate metrics from raw accel and temperature data."""
+
+import numpy as np
+import pandas as pd
 
 from actigrapy.common.data_model import InputData, OutputData
-import numpy as np
 
-def _calc_metrics(raw_data):
-    #calculate ENMO and angle z from raw data
-    #assumes a df 'raw_data' with columns 'X', 'Y', 'Z' with accelerometer data
+
+def calc_base_metrics(raw_data: pd.DataFrame) -> pd.DataFrame:
+    """Calculate the basic metrics, ENMO and angle z, from raw accelerometer data.
+
+    Args:
+        raw_data: The raw data containing columns 'X', 'Y', and 'Z' with
+        accelerometer data. 
+    
+    Returns:
+        pd.DataFrame: Returns a dataframe with ENMO and anglez columns. 
+    """
     ENMO_calc = np.asarray(np.sqrt(np.square(raw_data).sum(axis =1))-1)
     angle_z_raw = np.asarray(np.arctan(raw_data.Z / (np.sqrt( np.square(raw_data.X) + np.square(raw_data.Y)))) / (np.pi/180))
-    raw_data['ENMO'] = ENMO_calc
-    raw_data['Anglez'] = angle_z_raw
-    return raw_data
+    calc_data = pd.DataFrame()
+    calc_data['ENMO'] = ENMO_calc
+    calc_data['Anglez'] = angle_z_raw
+    return calc_data
+
+def down_sample(df: pd.DataFrame, signal_columns: list, sample_rate: int, ws: int
+                )-> pd.DataFrame:
+    """Downsample the input signal to a desired window size, in seconds.
+
+    Args:
+        df: The data frame containing the signals to downsample
+        signal_columns: List of column names to downsample
+        sample_rate: The sampling rate data was collected at
+        ws: The desired window size, in seconds, of the downsampled
+
+    Returns:
+        df_ds: dataframe with the downsampled signals in each column, labeled as 
+        downsample_{column}, where column is the same as the column name 
+        in signal_columns.
+    """
+    #define number of samples per window
+    samples_per_window= int(sample_rate * ws)
+
+    #initialize down_sample_pd
+    df_ds = pd.DataFrame()
+
+    #downsample each specified column
+    for column in signal_columns:
+        df_ds[f'downsampled_{column}'] = (df[column].groupby
+                        (df.index // samples_per_window).mean().reset_index(drop=True))
+    
+
+    return df_ds
--- src/actigrapy/io/loaders/gt3x.py
+++ src/actigrapy/io/loaders/gt3x.py
@@ -1,7 +1,9 @@
 """Loader for the .gt3x file format."""
 
 import os
+
 import numpy as np
+import pandas as pd
 import pygt3x.reader
 
 from actigrapy.common.data_model import InputData
@@ -10,18 +12,36 @@ from actigrapy.common.data_model import InputData
 def load(
     path: str | os.PathLike[str],
 ) -> InputData:
-    """Load actigraphy data from .gt3x file."""
+    """Load actigraphy data from .gt3x file.
+    
+    Args:
+        path: path to desired .gt3x file to process
+
+    Returns:
+        df: dataframe with loaded timestamps and acceleration data from .gt3x files
+        sample_rate: sampling rate in Hz
+    """
     with pygt3x.reader.FileReader(str(path)) as reader:
         df = reader.to_pandas()
     sample_rate = reader.info.sample_rate
-    #get temperature data
-    #temp = reader.temperature
+    #TODO get temperature
     return df, sample_rate
 
-def timefix(raw_data,sampling_rate):
-    """currently pygt3x does not save the ms data, we make the assumption that datapoints are saved
-    sequentially so that each new data point is one sample away from the previous
-    assumes the raw_data df and sampling rate in Hz as input """
+def timefix(raw_data: pd.DataFrame,sampling_rate: int) ->pd.DataFrame:
+    """Add ms data to timestamp data.
+    
+    Currently pygt3x does not save the ms data, we make the assumption that
+    datapoints are saved sequentially so that each new data point is one sample 
+    away from the previous.
+
+    Args:
+        raw_data: dataframe with original timestamp data from .gt3x reader
+        sampling_rate: sampling rate, in Hz, from .gt3x reader
+
+    Returns:
+        raw_data: added time column with corrected ms information and 
+        cast into datetime object
+    """
     timestamps = raw_data.index.to_numpy()
     time_fix = []
     for i in range(len(timestamps)):
