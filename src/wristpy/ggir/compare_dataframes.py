f6d74a3a9b665e90cb06fd81b30523c72de9972e
frey-perez <freymon.perez@childmind.org>
2024-02-28 11:42:28 -0500

    updated comments on compare_dataframes

--- src/wristpy/ggir/compare_dataframes.py
+++ src/wristpy/ggir/compare_dataframes.py
@@ -1,8 +1,11 @@
 """Comparing GGIR outputs to wristpy outputs for epoch 1."""
 
-import numpy as np
-import polars as pl
+from matplotlib import pyplot as plt 
+
 import pathlib
+
+import polars as pl
+
 from wristpy.common.data_model import OutputData
 
 
@@ -14,13 +17,22 @@ def compare(ggir_dataframe: pl.DataFrame, wristpy_dataframe: OutputData)-> pl.Da
     """Compares a wristpy and ggir dataframe.
 
     Args:
-        Two dataframes, one from each package
+        ggir_dataframe:
+            The GGIR derived dataframe to be used to calculate difference between GGIR 
+            and wristpy outputs.
+        wristpy_dataframe:
+            The wristpy OutputData object to be used to calculate difference between 
+            GGIR and wristpy outputs.
+        
 
     Returns:
-        A error/difference graph
+        A error/difference graph calculated by taking the difference between 
+        the wristpy OutputData and GGIR dataframe
     """
+    enmo_error = wristpy_dataframe.enmo_epoch1 - ggir_dataframe.enmo
+    anglez_error = wristpy_dataframe.anglez_epoch1 - ggir_dataframe.angelz
+
+    return enmo_error, anglez_error
 
 
-    error = np.abs(wristpy_dataframe- ggir_dataframe)
 
-    return error
