50a6aba5a792e38da0e3f1b81720e2ec02933ae0
frey-perez <freymon.perez@childmind.org>
2024-05-03 17:04:41 -0400

    Addressing PR comments
    -removed superfluous in-line comments
    -added additional assertions in unit test for time
    values and measure sizes
    -added an extra case to unit test were all 3 dims
    are none zero.

--- src/wristpy/processing/metrics.py
+++ src/wristpy/processing/metrics.py
@@ -5,23 +5,26 @@ import numpy as np
 from wristpy.core.models import Measurement
 
 
-def euclidean_norm(acceleration: Measurement)-> Measurement:
+def euclidean_norm_min_one(acceleration: Measurement) -> Measurement:
     """Compute ENMO, the Euclidean Norm Minus One (1 standard gravity unit).
+
+    Negative values of ENMO are set to zero because ENMO is meant as a measure of
+    physival activity. Negative values steming from low levels of activity or noise from
+    the device would have no meaningful interpretation in this context and would be
+    detrimental to the intended analysis.
 
     Args:
         acceleration: the three dimensional accelerometer data. A Measurement object,
-        it will have two attributes. 1) measurements, containing the three dimensional 
-        accelerometer data in an np.array and 2) time, a pl.Series containing 
+        it will have two attributes. 1) measurements, containing the three dimensional
+        accelerometer data in an np.array and 2) time, a pl.Series containing
         datetime.datetime objects.
 
     Returns:
-        A Measurement object containing the calculated ENMO values and the 
+        A Measurement object containing the calculated ENMO values and the
         associated time stamps taken from the input.
     """
-    #compute euclidean norm - 1
-    enmo = np.linalg.norm(acceleration.measurements, axis = 1) - 1
+    enmo = np.linalg.norm(acceleration.measurements, axis=1) - 1
 
-    #GGIR replaces negative values with 0
     enmo = np.maximum(enmo, 0)
 
-    return Measurement(measurements= enmo, time = acceleration.time)
+    return Measurement(measurements=enmo, time=acceleration.time)
--- tests/unit/test_metrics.py
+++ tests/unit/test_metrics.py
@@ -1,4 +1,5 @@
 """Testing functions of metrics module."""
+
 from datetime import datetime
 
 import numpy as np
@@ -6,34 +7,40 @@ import polars as pl
 import pytest
 
 from wristpy.core.models import Measurement
-from wristpy.processing.metrics import euclidean_norm
+from wristpy.processing.metrics import euclidean_norm_min_one
 
 TEST_LENGTH = 100
 
+
 @pytest.mark.parametrize(
-        "x,y,z, expected_enmo",
-        [
-            (np.zeros(TEST_LENGTH), np.zeros(TEST_LENGTH), np.zeros(TEST_LENGTH), 0),
-            (np.ones(TEST_LENGTH), np.zeros(TEST_LENGTH), np.zeros(TEST_LENGTH), 0),
-            (np.ones(TEST_LENGTH)*2, np.zeros(TEST_LENGTH), np.zeros(TEST_LENGTH), 1),
-            (np.ones(TEST_LENGTH)*3, np.zeros(TEST_LENGTH), np.zeros(TEST_LENGTH), 2)
-        ]
+    "x,y,z, expected_enmo",
+    [
+        (np.zeros(TEST_LENGTH), np.zeros(TEST_LENGTH), np.zeros(TEST_LENGTH), 0),
+        (np.ones(TEST_LENGTH), np.zeros(TEST_LENGTH), np.zeros(TEST_LENGTH), 0),
+        (np.ones(TEST_LENGTH) * 2, np.zeros(TEST_LENGTH), np.zeros(TEST_LENGTH), 1),
+        (np.ones(TEST_LENGTH), np.ones(TEST_LENGTH) * 2, np.ones(TEST_LENGTH) * 2, 2),
+    ],
 )
-def test_euclidean_norm(
-        x: np.ndarray, y: np.ndarray, z: np.ndarray, expected_enmo: float
-        )->None:
+def test_euclidean_norm_min_one(
+    x: np.ndarray, y: np.ndarray, z: np.ndarray, expected_enmo: float
+) -> None:
     """Tests the euclidean norm function."""
     dummy_date = datetime(2024, 5, 2)
     dummy_datetime = pl.Series("time", [dummy_date] * TEST_LENGTH)
-    #assemble Measurement object with test array and dummy datetime
     test_acceleration = Measurement(
-        measurements= np.column_stack((x, y, z)),
-        time = dummy_datetime)
+        measurements=np.column_stack((x, y, z)), time=dummy_datetime
+    )
 
-    enmo_results = euclidean_norm(test_acceleration)
+    enmo_results = euclidean_norm_min_one(test_acceleration)
 
-    
+    assert np.all(
+        np.isclose(enmo_results.measurements, expected_enmo)
+    ), f"Expected {expected_enmo}"
 
-    assert np.all( np.isclose(
-        enmo_results.measurements, expected_enmo
-        )), f"Expected {expected_enmo}"
+    assert enmo_results.time.equals(
+        test_acceleration.time
+    ), "Input time attribute does not match output time attribute."
+
+    assert enmo_results.measurements.shape == (
+        TEST_LENGTH,
+    ), f"Expected enmo shape: ({TEST_LENGTH},), got ({enmo_results.measurements.shape})"
