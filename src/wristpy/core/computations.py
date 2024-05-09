acf4466eb63f9d54e6af9759b7518474007987a7
Adam Santorelli <148909356+Asanto32@users.noreply.github.com>
2024-05-09 10:29:35 -0400

    Moving_mean implementation
    Moved mathematical functions into computations in the core as they will be accessed by various functions and removed them from metrics.py

--- src/wristpy/core/computations.py
+++ src/wristpy/core/computations.py
@@ -1,7 +1,5 @@
 """Calculate base metrics, enmo, anglez, and non-wear detection."""
 
-import warnings
-
 import polars as pl
 
 from wristpy.core.models import Measurement
@@ -10,26 +8,16 @@ from wristpy.core.models import Measurement
 def moving_mean(array: Measurement, epoch_length: int = 5) -> Measurement:
     """Calculate the moving mean of the sensor data in array.
 
-    The input must be a Measurement instance. The window size must be a posititve
-    non-zero integer. The function converts the epoch_length to a string that can
-    be used by polars group_by_dynamic. We then create a polars dataframe that is
-    a concatenation of the sensor data and the equivalent time data. We group
-    the data by creating a windowbased on the timestamp using the "every" function
-    and then take the mean of that group.
-
     Args:
         array: The Measurement object with the sensor data we want to take the mean of
-        epoch_length: The length of the window to calculate the moving mean over.
+        epoch_length: The length, in seconds, of the window.
 
     Returns:
         The moving mean of the array in a new Measurement instance.
+
+    Raises:
+        ValueError: If the epoch length is not an integer or is less than 1.
     """
-    if not isinstance(array, Measurement) or array.measurements.size == 0:
-        warnings.warn(
-            "Input is not a Measurement or is an empty Measurement. "
-            "Returning the input."
-        )
-        return array
     if not isinstance(epoch_length, int):
         raise ValueError("Epoch length must be an integer")
     if epoch_length < 1:
@@ -53,7 +41,11 @@ def moving_mean(array: Measurement, epoch_length: int = 5) -> Measurement:
         index_column="time", every=window_size_seconds
     ).agg(take_mean_expression)
 
+    measurements_mean_array = measurement_df_mean.drop("time").to_numpy()
+    if array.measurements.ndim == 1:
+        measurements_mean_array = measurements_mean_array.flatten()
+
     return Measurement(
-        measurements=measurement_df_mean.drop("time").to_numpy(),
+        measurements=measurements_mean_array,
         time=measurement_df_mean["time"],
     )
---
+++ tests/test_computations.py
@@ -0,0 +1,61 @@
+"""Test the function of computations module."""
+
+import numpy as np
+import pytest
+
+from wristpy.core import computations
+from wristpy.core.models import Measurement
+from wristpy.io.readers import readers
+
+
+def test_moving_mean_epoch_length_is_int() -> None:
+    """Test error for non-integer epoch length."""
+    tmp_data = np.array([1, 2, 3])
+    time = readers.unix_epoch_time_to_polars_datetime(np.array([1, 2, 3]), "s")
+    tmp_Measurement = Measurement(measurements=tmp_data, time=time)
+
+    with pytest.raises(ValueError):
+        computations.moving_mean(tmp_Measurement, epoch_length=5.5)
+
+
+def test_moving_mean_epoch_length_is_negative() -> None:
+    """Test error if the epoch length is negative."""
+    tmp_data = np.array([1, 2, 3])
+    time = readers.unix_epoch_time_to_polars_datetime(np.array([1, 2, 3]), "s")
+    tmp_Measurement = Measurement(measurements=tmp_data, time=time)
+
+    with pytest.raises(ValueError):
+        computations.moving_mean(tmp_Measurement, epoch_length=-5)
+
+
+def test_moving_mean_one_column() -> None:
+    """Test the functionality of the moving mean function for 1D Measurement."""
+    signal_length = 20
+    test_data = np.arange(0, signal_length)
+    test_time = readers.unix_epoch_time_to_polars_datetime(
+        np.arange(0, signal_length), "s"
+    )
+    expected_mean = np.array([2.0, 7.0, 12.0, 17.0])
+    test_measurement = Measurement(measurements=test_data, time=test_time)
+
+    test_measurement_mean = computations.moving_mean(test_measurement, epoch_length=5)
+
+    assert np.array_equal(test_measurement_mean.measurements, expected_mean)
+
+
+def test_moving_mean_three_columns() -> None:
+    """Test the functionality of the moving mean function for 3D Measurement."""
+    signal_length = 20
+    test_data = np.arange(0, signal_length * 3).reshape(signal_length, 3)
+    test_time = readers.unix_epoch_time_to_polars_datetime(
+        np.arange(0, signal_length), "s"
+    )
+    expected_mean = np.array(
+        ([[6.0, 7.0, 8.0], [21.0, 22.0, 23.0], [36.0, 37.0, 38.0], [51.0, 52.0, 53.0]])
+    )
+    test_measurement = Measurement(measurements=test_data, time=test_time)
+
+    test_measurement_mean = computations.moving_mean(test_measurement, epoch_length=5)
+
+    assert np.array_equal(test_measurement_mean.measurements, expected_mean)
+    assert test_measurement_mean.measurements.shape[1] == test_data.shape[1]
