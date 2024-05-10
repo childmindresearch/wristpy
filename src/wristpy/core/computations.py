b49536f1da09d2413a3a858fdca0ec078cc96dfb
Adam Santorelli <148909356+Asanto32@users.noreply.github.com>
2024-05-10 10:57:23 -0400

    Adding moving_std to computations
    Adding the moving_std function to computations. The structure, implementation, and tests are the same as moving_mean

--- src/wristpy/core/computations.py
+++ src/wristpy/core/computations.py
@@ -47,3 +47,47 @@ def moving_mean(array: Measurement, epoch_length: int = 5) -> Measurement:
         measurements=measurements_mean_array,
         time=measurement_df_mean["time"],
     )
+
+
+def moving_std(array: Measurement, epoch_length: int = 5) -> Measurement:
+    """Calculate the moving standard deviation (std) of the sensor data in array.
+
+    Args:
+        array: The Measurement object with the sensor data we want to take the std of
+        epoch_length: The length, in seconds, of the window.
+
+    Returns:
+        The moving std of the array in a new Measurement instance.
+
+    Raises:
+        ValueError: If the epoch length is not an integer or is less than 1.
+    """
+    if epoch_length < 1:
+        raise ValueError("Epoch length must be greater than 0")
+
+    window_size_seconds = str(epoch_length) + "s"
+
+    measurement_polars_df = pl.concat(
+        [pl.DataFrame(array.measurements), pl.DataFrame(array.time)], how="horizontal"
+    )
+
+    measurement_polars_df = measurement_polars_df.with_columns(
+        pl.col("time").set_sorted()
+    )
+
+    take_std_expression = [
+        pl.all().exclude(["time"]).drop_nans().std(),
+    ]
+
+    measurement_df_std = measurement_polars_df.group_by_dynamic(
+        index_column="time", every=window_size_seconds
+    ).agg(take_std_expression)
+
+    measurements_std_array = measurement_df_std.drop("time").to_numpy()
+    if array.measurements.ndim == 1:
+        measurements_std_array = measurements_std_array.flatten()
+
+    return Measurement(
+        measurements=measurements_std_array,
+        time=measurement_df_std["time"],
+    )
--- tests/test_computations.py
+++ tests/test_computations.py
@@ -7,6 +7,9 @@ from wristpy.core import computations
 from wristpy.core.models import Measurement
 from wristpy.io.readers import readers
 
+SIGNAL_LENGTH = 20
+EPOCH_LENGTH = 5
+
 
 def test_moving_mean_epoch_length_is_negative() -> None:
     """Test error if the epoch length is negative."""
@@ -15,33 +18,35 @@ def test_moving_mean_epoch_length_is_negative() -> None:
     tmp_Measurement = Measurement(measurements=tmp_data, time=time)
 
     with pytest.raises(ValueError):
-        computations.moving_mean(tmp_Measurement, epoch_length=-5)
+        computations.moving_mean(tmp_Measurement, epoch_length=-EPOCH_LENGTH)
 
 
 def test_moving_mean_one_column() -> None:
     """Test the functionality of the moving mean function for 1D Measurement."""
-    signal_length = 20
-    test_data = np.arange(0, signal_length)
+    test_data = np.arange(0, SIGNAL_LENGTH)
     test_time = readers.unix_epoch_time_to_polars_datetime(
-        np.arange(0, signal_length), "s"
+        np.arange(0, SIGNAL_LENGTH), "s"
     )
     expected_mean = np.array([2.0, 7.0, 12.0, 17.0])
     test_measurement = Measurement(measurements=test_data, time=test_time)
 
-    test_measurement_mean = computations.moving_mean(test_measurement, epoch_length=5)
+    test_measurement_mean = computations.moving_mean(
+        test_measurement, epoch_length=EPOCH_LENGTH
+    )
 
     assert np.allclose(test_measurement_mean.measurements, expected_mean)
     assert test_measurement_mean.measurements.ndim == expected_mean.ndim
-    assert np.isclose(test_measurement_mean.time.shape[0], (test_time.shape[0] / 5))
+    assert np.isclose(
+        test_measurement_mean.time.shape[0], (test_time.shape[0] / EPOCH_LENGTH)
+    )
 
 
 def test_moving_mean_three_columns() -> None:
     """Test the functionality of the moving mean function for three column array."""
-    signal_length = 20
-    epoch_length = 5
-    test_data = np.arange(0, signal_length * 3).reshape(signal_length, 3)
+
+    test_data = np.arange(0, SIGNAL_LENGTH * 3).reshape(SIGNAL_LENGTH, 3)
     test_time = readers.unix_epoch_time_to_polars_datetime(
-        np.arange(0, signal_length), "s"
+        np.arange(0, SIGNAL_LENGTH), "s"
     )
     expected_mean = np.array(
         ([[6.0, 7.0, 8.0], [21.0, 22.0, 23.0], [36.0, 37.0, 38.0], [51.0, 52.0, 53.0]])
@@ -49,11 +54,61 @@ def test_moving_mean_three_columns() -> None:
     test_measurement = Measurement(measurements=test_data, time=test_time)
 
     test_measurement_mean = computations.moving_mean(
-        test_measurement, epoch_length=epoch_length
+        test_measurement, epoch_length=EPOCH_LENGTH
     )
 
     assert np.allclose(test_measurement_mean.measurements, expected_mean)
     assert test_measurement_mean.measurements.shape[1] == test_data.shape[1]
     assert np.isclose(
-        test_measurement_mean.time.shape[0], (test_time.shape[0] / epoch_length)
+        test_measurement_mean.time.shape[0], (test_time.shape[0] / EPOCH_LENGTH)
+    )
+
+
+def test_moving_std_epoch_length_is_negative() -> None:
+    """Test error if the epoch length is negative."""
+    tmp_data = np.array([1, 2, 3])
+    time = readers.unix_epoch_time_to_polars_datetime(np.array([1, 2, 3]), "s")
+    tmp_Measurement = Measurement(measurements=tmp_data, time=time)
+
+    with pytest.raises(ValueError):
+        computations.moving_std(tmp_Measurement, epoch_length=-EPOCH_LENGTH)
+
+
+def test_moving_std_one_column() -> None:
+    """Test the functionality of the moving std function for 1D Measurement."""
+    test_data = np.ones(SIGNAL_LENGTH)
+    test_time = readers.unix_epoch_time_to_polars_datetime(
+        np.arange(0, SIGNAL_LENGTH), "s"
+    )
+    expected_std = np.array([0.0, 0.0, 0.0, 0.0])
+    test_measurement = Measurement(measurements=test_data, time=test_time)
+
+    test_measurement_std = computations.moving_std(
+        test_measurement, epoch_length=EPOCH_LENGTH
+    )
+
+    assert np.allclose(test_measurement_std.measurements, expected_std)
+    assert test_measurement_std.measurements.ndim == expected_std.ndim
+    assert np.isclose(
+        test_measurement_std.time.shape[0], (test_time.shape[0] / EPOCH_LENGTH)
+    )
+
+
+def test_moving_std_three_columns() -> None:
+    """Test the functionality of the moving std function for three column array."""
+    test_data = np.ones((SIGNAL_LENGTH, 3))
+    test_time = readers.unix_epoch_time_to_polars_datetime(
+        np.arange(0, SIGNAL_LENGTH), "s"
+    )
+    expected_std = np.zeros((4, 3))
+    test_measurement = Measurement(measurements=test_data, time=test_time)
+
+    test_measurement_std = computations.moving_std(
+        test_measurement, epoch_length=EPOCH_LENGTH
+    )
+
+    assert np.allclose(test_measurement_std.measurements, expected_std)
+    assert test_measurement_std.measurements.shape[1] == test_data.shape[1]
+    assert np.isclose(
+        test_measurement_std.time.shape[0], (test_time.shape[0] / EPOCH_LENGTH)
     )
