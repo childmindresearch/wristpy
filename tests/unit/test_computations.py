1b415d103e671d23a56f6adc513145c6e4edc3a5
frey-perez <160774979+frey-perez@users.noreply.github.com>
2024-05-24 13:55:23 -0400

    Update test_computations.py

--- tests/unit/test_computations.py
+++ tests/unit/test_computations.py
@@ -1,5 +1,6 @@
 """Test the function of computations module."""
 
+
 import numpy as np
 import pytest
 
@@ -26,6 +27,7 @@ def test_moving_mean_one_column() -> None:
     test_time = readers.unix_epoch_time_to_polars_datetime(
         np.arange(0, SIGNAL_LENGTH), "s"
     )
+    expected_time_shape = test_time.shape[0] / EPOCH_LENGTH
     expected_mean = np.array([2.0, 7.0, 12.0, 17.0])
     test_measurement = models.Measurement(measurements=test_data, time=test_time)
 
@@ -35,9 +37,7 @@ def test_moving_mean_one_column() -> None:
 
     assert np.allclose(test_measurement_mean.measurements, expected_mean)
     assert test_measurement_mean.measurements.ndim == expected_mean.ndim
-    assert np.isclose(
-        test_measurement_mean.time.shape[0], (test_time.shape[0] / EPOCH_LENGTH)
-    )
+    assert test_measurement_mean.time.shape[0] == expected_time_shape
 
 
 def test_moving_mean_three_columns() -> None:
@@ -46,6 +46,7 @@ def test_moving_mean_three_columns() -> None:
     test_time = readers.unix_epoch_time_to_polars_datetime(
         np.arange(0, SIGNAL_LENGTH), "s"
     )
+    expected_time_shape = test_time.shape[0] / EPOCH_LENGTH
     expected_mean = np.array(
         ([[6.0, 7.0, 8.0], [21.0, 22.0, 23.0], [36.0, 37.0, 38.0], [51.0, 52.0, 53.0]])
     )
@@ -57,9 +58,7 @@ def test_moving_mean_three_columns() -> None:
 
     assert np.allclose(test_measurement_mean.measurements, expected_mean)
     assert test_measurement_mean.measurements.shape[1] == test_data.shape[1]
-    assert np.isclose(
-        test_measurement_mean.time.shape[0], (test_time.shape[0] / EPOCH_LENGTH)
-    )
+    assert test_measurement_mean.time.shape[0] == expected_time_shape
 
 
 def test_moving_std_epoch_length_is_negative() -> None:
@@ -72,14 +71,25 @@ def test_moving_std_epoch_length_is_negative() -> None:
         computations.moving_std(tmp_Measurement, epoch_length=-EPOCH_LENGTH)
 
 
-def test_moving_std_one_column() -> None:
+@pytest.mark.parametrize(
+    "test_array, expected_std",
+    [
+        (np.ones(SIGNAL_LENGTH), np.array([0.0, 0.0, 0.0, 0.0])),
+        (
+            np.array([-2, -1, 0, 1, 2] * 4),
+            np.array([1.581139] * 4),
+        ),
+    ],
+)
+def test_moving_std_one_column(
+    test_array: np.ndarray, expected_std: np.ndarray
+) -> None:
     """Test the functionality of the moving std function for 1D Measurement."""
-    test_data = np.ones(SIGNAL_LENGTH)
     test_time = readers.unix_epoch_time_to_polars_datetime(
         np.arange(0, SIGNAL_LENGTH), "s"
     )
-    expected_std = np.array([0.0, 0.0, 0.0, 0.0])
-    test_measurement = models.Measurement(measurements=test_data, time=test_time)
+    expected_time_shape = int(test_time.shape[0] / EPOCH_LENGTH)
+    test_measurement = models.Measurement(measurements=test_array, time=test_time)
 
     test_measurement_std = computations.moving_std(
         test_measurement, epoch_length=EPOCH_LENGTH
@@ -87,18 +97,29 @@ def test_moving_std_one_column() -> None:
 
     assert np.allclose(test_measurement_std.measurements, expected_std)
     assert test_measurement_std.measurements.ndim == expected_std.ndim
-    assert np.isclose(
-        test_measurement_std.time.shape[0], (test_time.shape[0] / EPOCH_LENGTH)
-    )
+    assert test_measurement_std.time.shape[0] == expected_time_shape
 
 
-def test_moving_std_three_columns() -> None:
+@pytest.mark.parametrize(
+    "test_data, expected_std",
+    [
+        (np.ones(SIGNAL_LENGTH), np.array([0.0, 0.0, 0.0, 0.0])),
+        (
+            np.array([-2, -1, 0, 1, 2] * 4),
+            np.array([1.581139] * 4),
+        ),
+    ],
+)
+def test_moving_std_three_columns(
+    test_data: np.ndarray, expected_std: np.ndarray
+) -> None:
     """Test the functionality of the moving std function for three column array."""
-    test_data = np.ones((SIGNAL_LENGTH, 3))
+    test_data = np.column_stack([test_data] * 3)
     test_time = readers.unix_epoch_time_to_polars_datetime(
         np.arange(0, SIGNAL_LENGTH), "s"
     )
-    expected_std = np.zeros((4, 3))
+    expected_time_shape = int(test_time.shape[0] / EPOCH_LENGTH)
+    expected_std = np.column_stack([expected_std])
     test_measurement = models.Measurement(measurements=test_data, time=test_time)
 
     test_measurement_std = computations.moving_std(
@@ -107,6 +128,4 @@ def test_moving_std_three_columns() -> None:
 
     assert np.allclose(test_measurement_std.measurements, expected_std)
     assert test_measurement_std.measurements.shape[1] == test_data.shape[1]
-    assert np.isclose(
-        test_measurement_std.time.shape[0], (test_time.shape[0] / EPOCH_LENGTH)
-    )
+    assert test_measurement_std.time.shape[0] == expected_time_shape
