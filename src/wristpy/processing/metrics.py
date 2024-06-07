edf279aff3e65f054dc320d28a715df55d162ea4
Adam Santorelli <148909356+Asanto32@users.noreply.github.com>
2024-06-07 13:24:20 -0400

    feat: implementing non-wear detection
    Initial implementation of the non-wear detection algorithm that matches GGIR 2023 implementation.
    First version of unit_tests

--- src/wristpy/processing/metrics.py
+++ src/wristpy/processing/metrics.py
@@ -1,6 +1,7 @@
 """Calculate base metrics, anglez and enmo."""
 
 import numpy as np
+import polars as pl
 
 from wristpy.core import models
 
@@ -53,3 +54,122 @@ def angle_relative_to_horizontal(
     angle_degrees = np.degrees(angle_radians)
 
     return models.Measurement(measurements=angle_degrees, time=acceleration.time)
+
+
+def detect_nonwear(
+    acceleration: models.Measurement,
+    short_epoch_length: int = 900,
+    long_epoch_length: int = 3600,
+    std_criteria: float = 0.013,
+    range_criteria: float = 0.05,
+) -> models.Measurement:
+    """Set non_wear_flag based on accelerometer data.
+
+    This implements GGIR "2023" non-wear detection algorithm.
+    Briefly, the algorithm, creates a sliding window of long epoech length that steps
+    forward by the short epoch length.
+    It checks if the acceleration data in that long window, for each axis, meets certain
+    criteria thresholds to compute a non-wear value.
+    And then applies that non-wear value to all the short windows that make up the long
+    window. Additionally, for overlapping windows, the maximum of the non-wear value for
+    the overlaps is kept. Finally, there is a pass to find isolated "1s" in the non-wear
+    value, and set them to 2 if surrounded by > 1 values. The non-wear flag is set to
+    1 (true) if the non-wear value is >= 2, and 0 (false) otherwise.
+
+
+    Args:
+        acceleration: The Measurment instance that contains the calibrated acceleration
+        data
+        short_epoch_length: The short window size, in seconds, for non-wear detection
+        long_epoch_length: The long window size, in seconds, for non-wear detection
+        std_criteria: Threshold criteria for standard deviation
+        range_criteria: Threshold criteria for range of acceleration
+
+
+    Returns:
+        A new Measurment instance with the non-wear flag and corresponding timestamps.
+    """
+    n_short_epoch_in_long_epoch = int(long_epoch_length / short_epoch_length)
+
+    acceleration_polars_df = pl.DataFrame(
+        {
+            "X": acceleration.measurements[:, 0],
+            "Y": acceleration.measurements[:, 1],
+            "Z": acceleration.measurements[:, 2],
+            "time": acceleration.time,
+        }
+    )
+    acceleration_polars_df = acceleration_polars_df.with_columns(
+        pl.col("time").set_sorted()
+    )
+
+    acceleration_grouped_by_short_window = acceleration_polars_df.group_by_dynamic(
+        index_column="time", every=(str(short_epoch_length) + "s")
+    ).agg([pl.all().exclude(["time"])])
+
+    nonwear_value_array = np.zeros(len(acceleration_grouped_by_short_window))
+
+    for window_n in range(
+        len(acceleration_grouped_by_short_window) - n_short_epoch_in_long_epoch + 1
+    ):
+        acceleration_selected_long_window = acceleration_grouped_by_short_window[
+            window_n : window_n + n_short_epoch_in_long_epoch
+        ]
+
+        calculated_nonwear_value = acceleration_selected_long_window.select(
+            pl.col("X", "Y", "Z").map_batches(
+                lambda df: _compute_nonwear_value_per_axis(
+                    df, std_criteria, range_criteria
+                )
+            )
+        ).sum_horizontal()
+
+        max_window_value = np.maximum(
+            nonwear_value_array[window_n : window_n + n_short_epoch_in_long_epoch],
+            np.repeat(calculated_nonwear_value, n_short_epoch_in_long_epoch),
+        )
+        nonwear_value_array[window_n : window_n + n_short_epoch_in_long_epoch] = (
+            max_window_value
+        )
+
+    nonwear_ones = np.where(nonwear_value_array == 1)[0]
+
+    for ones_index in nonwear_ones:
+        if ones_index == 0:
+            continue
+        if ones_index == len(nonwear_value_array) - 1:
+            continue
+        if (nonwear_value_array[ones_index - 1] > 1) and (
+            nonwear_value_array[ones_index + 1] > 1
+        ):
+            nonwear_value_array[ones_index] = 2
+
+    non_wear_flag = np.where(nonwear_value_array >= 2, 1, 0)
+
+    return models.Measurement(
+        measurements=non_wear_flag, time=acceleration_grouped_by_short_window["time"]
+    )
+
+
+def _compute_nonwear_value_per_axis(
+    axis_acceleration_data: pl.Series, std_criteria: float, range_criteria: float
+) -> int:
+    """Helper function to calculate the nonwear criteria per axis.
+
+    Args:
+        axis_acceleration_data: The long window acceleration data for one axis.
+        It is polars.Series chunked into short windows where each row is a list of the
+        acceleration data of one axis (length of each list is the number of samples
+        that make up short_epoch_length in seconds).
+        std_criteria: Threshold criteria for standard deviation
+        range_criteria: Threshold criteria for range of acceleration
+
+    Returns:
+        Non-wear value for the axis.
+    """
+    axis_long_window_data = pl.concat(axis_acceleration_data, how="vertical")
+    axis_std = axis_long_window_data.std()
+    axis_range = axis_long_window_data.max() - axis_long_window_data.min()
+
+    criteria_boolean = (axis_std < std_criteria) & (axis_range < range_criteria)
+    return int(criteria_boolean)
--- tests/unit/test_metrics.py
+++ tests/unit/test_metrics.py
@@ -1,6 +1,6 @@
 """Testing functions of metrics module."""
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import numpy as np
 import polars as pl
@@ -10,6 +10,23 @@ from wristpy.core import models
 from wristpy.processing import metrics
 
 TEST_LENGTH = 100
+
+
+@pytest.fixture(scope="module")
+def create_acceleration() -> pl.DataFrame:
+    """Fixture to create a dummy acceleration DataFrame to be used in multiple tests."""
+    dummy_date = datetime(2024, 5, 2)
+    dummy_datetime_list = [dummy_date + timedelta(seconds=i) for i in range(10000)]
+    test_time = pl.Series("time", dummy_datetime_list)
+    acceleration_polars_df = pl.DataFrame(
+        {
+            "X": np.ones(10000),
+            "Y": np.ones(10000),
+            "Z": np.ones(10000),
+            "time": test_time,
+        }
+    )
+    return acceleration_polars_df
 
 
 @pytest.mark.parametrize(
@@ -79,3 +96,64 @@ def test_angle_relative_to_horizontal(
     assert angle_z_results.measurements.shape == (1,), (
         f"Expected anglez shape: {(1,)}," f"got({angle_z_results.measurements.shape})"
     )
+
+
+@pytest.mark.parametrize(
+    "modifier, expected_result",
+    [
+        (1, 1),
+        (0, 0),
+    ],
+)
+def test_compute_nonwear_value_per_axis(
+    create_acceleration: pl.DataFrame, modifier: int, expected_result: int
+) -> None:
+    """Test the nonwear value per axis function."""
+    std_criteria = modifier
+    range_criteria = modifier
+    acceleration = create_acceleration
+    acceleration = acceleration.with_columns(pl.col("time").set_sorted())
+    acceleration_grouped = acceleration.group_by_dynamic(
+        index_column="time", every="5s"
+    ).agg([pl.all().exclude(["time"])])
+
+    test_result = metrics._compute_nonwear_value_per_axis(
+        acceleration_grouped["X"], std_criteria, range_criteria
+    )
+
+    assert (
+        test_result == expected_result
+    ), f"Expected {expected_result}, got: {test_result}"
+
+
+@pytest.mark.parametrize(
+    "modifier, expected_result",
+    [
+        (1, 1),
+        (0, 0),
+    ],
+)
+def test_detect_nonwear(
+    create_acceleration: pl.DataFrame, modifier: int, expected_result: int
+) -> None:
+    """Test the detect nonwear function."""
+    short_epoch_length = 5
+    long_epoch_length = 20
+    std_criteria = modifier
+    range_criteria = modifier
+    acceleration_df = create_acceleration
+    acceleration = models.Measurement(
+        measurements=acceleration_df.select(["X", "Y", "Z"]).to_numpy(),
+        time=acceleration_df["time"],
+    )
+
+    test_result = metrics.detect_nonwear(
+        acceleration,
+        short_epoch_length,
+        long_epoch_length,
+        std_criteria,
+        range_criteria,
+    )
+    assert np.all(
+        test_result.measurements == modifier
+    ), f"Expected non-wear flag value to be {expected_result}, got: {test_result}"
