b3e3b1fd29ceb63d5f32c1467f8b27c097644281
frey-perez <160774979+frey-perez@users.noreply.github.com>
2024-06-25 09:14:06 -0400

    Applying GGIR autocalibration and tests.

--- src/wristpy/processing/calibration.py
+++ src/wristpy/processing/calibration.py
@@ -1,6 +1,6 @@
 """Calibrate accelerometer data."""
 
-from typing import Optional
+import math
 
 import numpy as np
 from sklearn import linear_model
@@ -26,20 +26,51 @@ class CalibrationError(Exception):
         super().__init__(self.message)
 
 
-class InsufficientHours(Exception):
-    """Raises error when not enough data hours."""
-
-    def __init__(self, min_calibration_hours: int, total_hours: int) -> None:
-        """Init."""
-        self.message = (
-            f"Calibration requires {min_calibration_hours} hours, "
-            f"only {total_hours} hours available."
-        )
-        super().__init__(self.message)
-
-
 class Calibration:
-    """Calibration."""
+    """Implements calibration on accelerometer data, based off of GGIR's implementation.
+
+    This class implements methods for autocalibrating accelerometer data using either
+    entire dataset or subsets, as determined by the settings. Depending on the settings
+    a scale and offset value is determined and applied to the data which, if
+    successful, is returned to the user.
+
+    Attributes.
+        chunked (bool): Determines if either entire dataset will be used, or if
+        calibration will be attempted with a subset of the data. Set to false by
+        default, when true will initiate _chunked_calibration from the run() method
+        instead of standard _calibration.
+
+        min_acceleration (float): The value on either side of 0g for each axis.
+        Determines if sphere is sufficiently populated to obtain meaningful calibration
+        result. Default is 0.3g.
+
+        min_calibration_hours (int): The minimum amount of data in hours needed to
+        preform calibration. Default is 72. If chunked calibration is selected this will
+        be the size of the initial subset taken for calibration.If error has not been
+        reduced below the min_error value, an additional 12 hours will be taken until
+        all data is used or calibration is successful.
+
+        min_standard_deviation (float): The standard deviation critieria used to select
+        portions of the data with no movement. Rolling windows with standard deviations
+        below this value will be determined has "still". This value is likely to be
+        different between devices. Default is 0.013g, the value determined for GeneActiv
+        devices. If measuring the noise in a bench-top test, this threshold should be
+        about `1.2 * noise`.
+
+        max_iterations (int): The maximum amount of iterations for the closest_point_fit
+        method. Default is 1000, generally should be left at this value.
+
+        error_tolerance (float): Tolerated level of error, when the closest_point_fit
+        method arrives at this value or better, the process ends. Default is 1e-10.
+        Generally should be left at this value.
+
+        min_error (float): Minimum acceptable error. If calibration can not reach this
+        threshold it will error.
+
+
+
+
+    """
 
     def __init__(
         self,
@@ -50,9 +81,31 @@ class Calibration:
         max_iterations: int = 1000,
         error_tolerance: float = 1e-10,
         min_error: float = 0.01,
-        sampling_rate_hz: int = 30,
     ) -> None:
-        """init."""
+        """init.
+
+        Attributes:
+            chunked (bool, optional): Whether to use chunked calibration for long
+            recordings.
+
+            min_acceleration (float,optional): Minimum acceleration for sphere criteria.
+
+            min_calibration_hours (int,optional): Minimum hours of data required for
+            calibration.
+
+            min_standard_deviation (float,optional): Minimum standard deviation for
+            no-motion detection.
+
+            max_iterations (int,optional): Maximum number of iterations for
+            optimization.
+
+            error_tolerance (float,optional): Tolerance for optimization convergence.
+
+            min_error (float,optional): Minimum acceptable calibration error.
+
+        Returns:
+            None
+        """
         self.chunked = chunked
         self.min_acceleration = min_acceleration
         self.min_calibration_hours = min_calibration_hours
@@ -60,11 +113,143 @@ class Calibration:
         self.max_iterations = max_iterations
         self.error_tolerance = error_tolerance
         self.min_error = min_error
-        self.sampling_rate_hz: int = sampling_rate_hz
         self.current_iteration = 0
 
-    def _calibrate(self, acceleration: models.Measurement) -> models.Measurement:
-        """calibrates."""
+    def run(self, acceleration: models.Measurement) -> models.Measurement:
+        """Runs acceleration data of the Measurement class based on the settings.
+
+        Chunks the data if chunked = true, otherwise it preforms calibration on the
+        entire dataset. linear transformation is then applied to the data.
+
+        Args:
+            acceleration(Measurement): the accelerometer data containing x,y,z axis
+            data and time stamps.
+
+        Returns:
+            A Measurement object which the original input data, with a linear
+            transformation applied.
+        """
+        data_range = acceleration.time.max() - acceleration.time.min()
+        total_hours = math.ceil(data_range.total_seconds() / 3600)
+
+        if total_hours < self.min_calibration_hours:
+            raise ValueError(
+                f"Calibration requires {self.min_calibration_hours} hours",
+                f"but only {total_hours} hours of data were given.",
+            )
+
+        if self.chunked:
+            linear_trans = self._chunked_calibration(acceleration=acceleration)
+        else:
+            linear_trans = self._calibrate(acceleration=acceleration)
+
+        transformed_data = (
+            acceleration.measurements * linear_trans["scale"]
+        ) + linear_trans["offset"]
+
+        return models.Measurement(measurements=transformed_data, time=acceleration.time)
+
+    def _chunked_calibration(
+        self, acceleration: models.Measurement
+    ) -> dict[str, float]:
+        """Chunks the data into subsets, to calibrate on smaller sections of data.
+
+        Args:
+            acceleration (Measurement): the accelerometer data containing x,y,z axis
+            data and time stamps.
+
+        Returns:
+            A dictionary with type str:float. Contains two keys labeled `scale` and
+            `offset`.
+        """
+        chunk_num = 0
+        finished = False
+
+        while not finished:
+            subset = self._take_chunk(acceleration=acceleration, chunk_num=chunk_num)
+            if len(acceleration.measurements) == len(subset.measurements):
+                finished = True
+
+            try:
+                return self._calibrate(subset)
+            except (SphereCriteriaError, CalibrationError, ValueError):
+                chunk_num += 1
+
+        raise CalibrationError
+
+    def _take_chunk(
+        self, acceleration: models.Measurement, chunk_num: int
+    ) -> models.Measurement:
+        """Takes the next chunk based on the chunk number.
+
+        Args:
+            acceleration (Measurement): the accelerometer data containing x,y,z axis
+            data and time stamps.
+
+            chunk_num (int): the current 12 hour chunk iteration being taken.
+
+        Returns:
+            The next chunk of the accelerometer data, if this is the final chunk, the
+            entire data is returned.
+        """
+        sampling_rate = self._get_sampling_rate(acceleration=acceleration)
+        min_samples = int(self.min_calibration_hours * 3600 * sampling_rate)
+        chunk_size = int(12 * 3600 * sampling_rate)
+
+        total_samples = len(acceleration.measurements)
+        current_sample = min_samples + (chunk_num * chunk_size)
+
+        if current_sample > total_samples:
+            current_sample = total_samples
+
+        return models.Measurement(
+            measurements=acceleration.measurements[:current_sample, :],
+            time=acceleration.time[:current_sample],
+        )
+
+    def _calibrate(self, acceleration: models.Measurement) -> dict[str, float]:
+        """Calibrates data and returns scale and offset values.
+
+        If error is low enough, the linear transformation is returned in a dict,
+        errors if not.
+
+        Args:
+            acceleration (Measurement): the accelerometer data containing x,y,z axis
+            data and time stamps.
+
+        Returns:
+            A dictionary with type str:float. Contains two keys labeled `scale` and
+            `offset`.
+        """
+        no_motion_data = self._extract_no_motion(acceleration=acceleration)
+        linear_tranformation = self._closest_point_fit(no_motion_data=no_motion_data)
+
+        no_motion_calibrated = (
+            no_motion_data * linear_tranformation["scale"]
+        ) + linear_tranformation["offset"]
+
+        cal_error_initial = np.round(
+            np.mean(abs(np.linalg.norm(no_motion_data, axis=1) - 1)), decimals=5
+        )
+        cal_error_end = np.around(
+            np.mean(abs(np.linalg.norm(no_motion_calibrated, axis=1) - 1)), decimals=5
+        )
+
+        if (cal_error_end >= cal_error_initial) or (cal_error_end >= self.min_error):
+            raise CalibrationError
+
+        return linear_tranformation
+
+    def _extract_no_motion(self, acceleration: models.Measurement) -> np.ndarray:
+        """Identifies areas of stillness using standard deviation and mean.
+
+        Args:
+            acceleration (Measurement): the accelerometer data containing x,y,z axis
+            data and time stamps.
+
+        Returns:
+            an ndarray containing the accelerometer data determined to have no motion.
+        """
         moving_sd = computations.moving_std(acceleration, 10)
         moving_mean = computations.moving_mean(acceleration, 10)
 
@@ -72,10 +257,12 @@ class Calibration:
             moving_sd.measurements < self.min_standard_deviation, axis=1
         ) & np.all(np.abs(moving_mean.measurements) < 2, axis=1)
 
-        offset = np.zeros(3)
-        scale = np.ones(3)
-
         no_motion_data = moving_mean.measurements[no_motion_check]
+
+        if no_motion_data.shape[0] == 0:
+            raise ValueError(
+                "Zero non-motion epochs found. Data did not meet criteria."
+            )
 
         sphere_criteria_check = np.sum(
             (no_motion_data.min(axis=0) < -self.min_acceleration)
@@ -83,16 +270,27 @@ class Calibration:
         )
         if sphere_criteria_check != 3:
             raise SphereCriteriaError
+        else:
+            return no_motion_data
 
+    def _closest_point_fit(self, no_motion_data: np.ndarray) -> dict[str, float]:
+        """Applies closest point fit to no motion data.
+
+        Args:
+            no_motion_data (np.ndarray): The acceleration data during periods of no
+            motion, in order to determine scale and offset.
+
+        Returns:
+            A dictionary with type str:float. Contains two keys labeled `scale` and
+            `offset`.
+        """
         weights = np.ones(no_motion_data.shape[0]) * 100
         residual = [np.Inf]
 
         linear_regression_model = linear_model.LinearRegression()
 
-        cal_error_initial = np.round(
-            np.mean(abs(np.linalg.norm(no_motion_data, axis=1) - 1)), decimals=5
-        )
-
+        offset = np.zeros(3)
+        scale = np.ones(3)
         for i in range(self.max_iterations):
             current = (no_motion_data * scale) + offset
             closest_point = current / np.linalg.norm(current, axis=1, keepdims=True)
@@ -124,64 +322,19 @@ class Calibration:
             if abs(residual[i] - residual[i - 1]) < self.error_tolerance:
                 break
 
-        no_motion_calibrated = (no_motion_data * scale) + offset
-        cal_error_end = np.around(
-            np.mean(abs(np.linalg.norm(no_motion_calibrated, axis=1) - 1)), decimals=5
-        )
-
-        if (cal_error_end >= cal_error_initial) or (cal_error_end >= self.min_error):
-            raise CalibrationError
-
         return {"scale": scale, "offset": offset}
 
-    def _chunk(self, acceleration: models.Measurement) -> Optional[models.Measurement]:
-        """Chunk data until you have enough to calibrate."""
-        min_samples = int(self.min_calibration_hours * 3600 * self.sampling_rate_hz)
-        chunk_size = int(12 * 3600 * self.sampling_rate_hz)
+    def _get_sampling_rate(self, acceleration: models.Measurement) -> int:
+        """Get the sampling rate.
 
-        chunk_num = 0
+        Args:
+            acceleration (Measurement): the accelerometer data containing x,y,z axis
+            data and time stamps.
 
-        total_samples = acceleration.measurements.shape[0]
-        current_samples = min_samples
-
-        while current_samples <= total_samples:
-            accel_data_trimmed = acceleration.measurements[
-                : min_samples + (chunk_size * chunk_num), :
-            ]
-            time_data_trimmed = acceleration.time[
-                : min_samples + (chunk_size * chunk_num)
-            ]
-            try:
-                return self._calibrate(
-                    models.Measurement(
-                        measurements=accel_data_trimmed, time=time_data_trimmed
-                    )
-                )
-            except (SphereCriteriaError, CalibrationError):
-                chunk_num += 1
-                current_samples += chunk_size
-
-        raise CalibrationError
-
-    def run(self, acceleration: models.Measurement) -> Optional[models.Measurement]:
-        """Based on the settings run."""
-        data_range = acceleration.time.max() - acceleration.time.min()
-        total_hours = data_range.total_seconds() / 3600
-
-        if total_hours < self.min_calibration_hours:
-            raise InsufficientHours(self.min_calibration_hours, total_hours)
-
-        try:
-            if self.chunked:
-                linear_trans = self._chunk(acceleration)
-            else:
-                linear_trans = self._calibrate(acceleration=acceleration)
-        except (SphereCriteriaError, CalibrationError) as e:
-            print(e)
-            return None
-
-        transformed_data = (
-            acceleration.measurements * linear_trans["scale"]
-        ) + linear_trans["offset"]
-
-        return models.Measurement(measurements=transformed_data, time=acceleration.time)
+        Returns:
+            sampling rate in Hz.
+        """
+        sampling_rate = acceleration.time.len() / round(
+            (acceleration.time.max() - acceleration.time.min()).total_seconds()
+        )
+        return round(sampling_rate)
---
+++ tests/unit/test_calibration.py
@@ -0,0 +1,239 @@
+"""test the calibration module."""
+
+from datetime import datetime, timedelta
+from unittest import mock
+
+import numpy as np
+import polars as pl
+import pytest
+
+from wristpy.core import models
+from wristpy.processing import calibration
+
+
+def create_dummy_measurement(
+    sampling_rate: int, duration_hours: int = 84, all_same_num: float | None = None
+) -> models.Measurement:
+    """Create dummy measurement."""
+    n_samples = sampling_rate * 3600 * duration_hours
+    start_time = datetime.now()
+    delta = timedelta(seconds=1 / sampling_rate)
+
+    time_data = [start_time + i * delta for i in range(n_samples)]
+
+    if all_same_num is not None:
+        measurement_data = np.full((n_samples, 3), (all_same_num))
+    else:
+        measurement_data = np.random.normal(loc=1.0, scale=0.1, size=(n_samples, 3))
+
+    dummy_measurement = models.Measurement(
+        measurements=measurement_data, time=pl.Series(time_data).alias("time")
+    )
+
+    return dummy_measurement
+
+
+def test_get_sampling_rate() -> None:
+    """Find sampling rate."""
+    dummy_measure = create_dummy_measurement(
+        sampling_rate=60, duration_hours=1, all_same_num=0.0
+    )
+    calibrator = calibration.Calibration()
+
+    sampling_rate = calibrator._get_sampling_rate(dummy_measure)
+
+    assert sampling_rate == 60
+
+
+def test_extract_no_motion_value_error() -> None:
+    """Check extract no motion."""
+    date = datetime.now()
+    delta = timedelta(seconds=1)
+    dummy_datetime = [date + (i * delta) for i in range(60)]
+    dummy_data = np.random.uniform(-0.29, 0.29, (60, 3))
+    dummy_measure = models.Measurement(
+        measurements=dummy_data, time=pl.Series(dummy_datetime).alias("time")
+    )
+    calibrator = calibration.Calibration()
+
+    with pytest.raises(
+        ValueError, match="Zero non-motion epochs found. Data did not meet criteria."
+    ):
+        calibrator._extract_no_motion(acceleration=dummy_measure)
+
+
+def test_extract_no_motion_sphere_error() -> None:
+    """Check extract no motion for not meeting sphere error."""
+    dummy_measure = create_dummy_measurement(
+        sampling_rate=1, duration_hours=1, all_same_num=0.29
+    )
+    calibrator = calibration.Calibration()
+
+    with pytest.raises(calibration.SphereCriteriaError):
+        calibrator._extract_no_motion(acceleration=dummy_measure)
+
+
+def test_closest_point_fit() -> None:
+    """Test closest point fit."""
+    dummy_no_motion = np.full((100, 3), (1 / np.sqrt(3)))
+    calibrator = calibration.Calibration()
+
+    linear_transform = calibrator._closest_point_fit(dummy_no_motion)
+
+    assert np.allclose(linear_transform["scale"], 1.0)
+    assert np.allclose(linear_transform["offset"], 0.0)
+
+
+def test_calibrate_calibration_error() -> None:
+    """Test failure due to calibration error."""
+    no_motion_data = np.full((100, 3), (1 / np.sqrt(3))) + 0.3
+    dummy_measure = create_dummy_measurement(
+        sampling_rate=1, duration_hours=1, all_same_num=(1 / np.sqrt(3)) + 0.3
+    )
+    calibrator = calibration.Calibration(min_error=0.0001, max_iterations=5)
+
+    with mock.patch.object(
+        calibration.Calibration, "_extract_no_motion"
+    ) as mock_extracted_data:
+        mock_extracted_data.return_value = no_motion_data
+
+        with pytest.raises(calibration.CalibrationError):
+            calibrator._calibrate(dummy_measure)
+
+
+def test_calibration_successful() -> None:
+    """Test successful calibration."""
+    no_motion_data = np.full((100, 3), (1 / np.sqrt(3) + 1))
+    linear_transformation = {"scale": 1, "offset": -1}
+    dummy_measure = create_dummy_measurement(
+        sampling_rate=2, duration_hours=1, all_same_num=(1 / np.sqrt(3) + 1)
+    )
+
+    with mock.patch.object(
+        calibration.Calibration, "_extract_no_motion"
+    ) as mock_extracted_data:
+        with mock.patch.object(
+            calibration.Calibration, "_closest_point_fit"
+        ) as mock_closest_point_fit:
+            mock_extracted_data.return_value = no_motion_data
+            mock_closest_point_fit.return_value = linear_transformation
+            calibrator = calibration.Calibration()
+
+            result = calibrator._calibrate(dummy_measure)
+
+            assert result["scale"] == linear_transformation["scale"]
+            assert result["offset"] == linear_transformation["offset"]
+
+
+@pytest.mark.parametrize(
+    "chunk_num, expected_len",
+    [(0, (3600 * 72)), ((1), (3600 * 84)), ((2), (3600 * 84))],
+)
+def test_take_chunk(chunk_num: int, expected_len: int) -> None:
+    """Test take chunk."""
+    dummy_measure = create_dummy_measurement(sampling_rate=1, duration_hours=84)
+    dummy_calibrator = calibration.Calibration()
+
+    subset = dummy_calibrator._take_chunk(
+        acceleration=dummy_measure, chunk_num=chunk_num
+    )
+
+    assert len(subset.measurements) == expected_len
+    assert len(subset.time) == expected_len
+
+
+def test_chunked_calibration_fail_and_succeed() -> None:
+    """Test chunked calibration failure, then success after taking a chunk."""
+    dummy_measurement = create_dummy_measurement(sampling_rate=1, duration_hours=84)
+    mock_linear_transformation = {
+        "scale": np.array([1, 1, 1]),
+        "offset": np.array([0, 0, 0]),
+    }
+
+    calibrator = calibration.Calibration(chunked=True, min_calibration_hours=72)
+
+    with mock.patch.object(calibration.Calibration, "_calibrate") as mock_calibrate:
+        mock_calibrate.side_effect = [
+            calibration.CalibrationError(),
+            mock_linear_transformation,
+        ]
+
+        result = calibrator._chunked_calibration(dummy_measurement)
+
+        assert np.allclose(result["scale"], mock_linear_transformation["scale"])
+        assert np.allclose(result["offset"], mock_linear_transformation["offset"])
+        assert mock_calibrate.call_count == 2
+
+        first_call_samples = mock_calibrate.call_args_list[0][0][0].measurements.shape[
+            0
+        ]
+        second_call_samples = mock_calibrate.call_args_list[1][0][0].measurements.shape[
+            0
+        ]
+        assert first_call_samples == (3600 * 72)
+        assert second_call_samples == (3600 * 84)
+
+
+def test_run_hours_value_error() -> None:
+    """Test error when not enough hours of data."""
+    dummy_measure = create_dummy_measurement(sampling_rate=60, duration_hours=10)
+    calibrator = calibration.Calibration(min_calibration_hours=72)
+
+    with pytest.raises(ValueError):
+        calibrator.run(dummy_measure)
+
+
+@pytest.mark.parametrize(
+    "scale, offset",
+    [([1.0, 1.0, 1.0], [0.0, 0.0, 0.0]), ([2.0, 2.0, 2.0], [1.0, 1.0, 1.0])],
+)
+def test_run_successful_calibration(scale: list[float], offset: list[float]) -> None:
+    """Test the run method when calibration succeeds."""
+    dummy_measure = create_dummy_measurement(sampling_rate=1, duration_hours=24)
+
+    calibrator = calibration.Calibration(min_calibration_hours=12)
+
+    mock_transformation = {
+        "scale": np.array(scale),
+        "offset": np.array(offset),
+    }
+    expected_data = (
+        dummy_measure.measurements * mock_transformation["scale"]
+    ) + mock_transformation["offset"]
+    with mock.patch.object(
+        calibration.Calibration, "_calibrate", return_value=mock_transformation
+    ):
+        result = calibrator.run(dummy_measure)
+
+    assert isinstance(result, models.Measurement)
+    assert np.allclose(result.measurements, expected_data)
+
+
+@pytest.mark.parametrize(
+    "scale, offset",
+    [([1.0, 1.0, 1.0], [0.0, 0.0, 0.0]), ([2.0, 2.0, 2.0], [1.0, 1.0, 1.0])],
+)
+def test_run_successful_chunked_calibration(
+    scale: list[float], offset: list[float]
+) -> None:
+    """Test chunked calibration when calibration succeeds."""
+    dummy_measure = create_dummy_measurement(sampling_rate=1, duration_hours=24)
+
+    calibrator = calibration.Calibration(min_calibration_hours=12, chunked=True)
+
+    mock_transformation = {
+        "scale": np.array(scale),
+        "offset": np.array(offset),
+    }
+    expected_data = (
+        dummy_measure.measurements * mock_transformation["scale"]
+    ) + mock_transformation["offset"]
+    with mock.patch.object(
+        calibration.Calibration,
+        "_chunked_calibration",
+        return_value=mock_transformation,
+    ):
+        result = calibrator.run(dummy_measure)
+
+    assert isinstance(result, models.Measurement)
+    assert np.allclose(result.measurements, expected_data)
