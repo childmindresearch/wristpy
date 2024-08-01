6e2e9a90625291c1069e09cf2fe0e3dacdd10ae9
frey-perez <160774979+frey-perez@users.noreply.github.com>
2024-08-01 11:52:17 -0400

    Added second test to hit first return statement.

--- tests/unit/test_logger.py
+++ tests/unit/test_logger.py
@@ -17,3 +17,13 @@ def test_get_logger(caplog: pytest.LogCaptureFixture) -> None:
     assert "Debug message here" not in caplog.text
     assert "Info message here." in caplog.text
     assert "Warning message here." in caplog.text
+
+
+def test_get_logger_second_call() -> None:
+    """Test get logger when a handler already exists."""
+    logger = config.get_logger()
+    second_logger = config.get_logger()
+
+    assert len(logger.handlers) == len(second_logger.handlers) == 1
+    assert logger.handlers[0] is second_logger.handlers[0]
+    assert logger is second_logger
