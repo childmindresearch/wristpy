6d888cb95b99006cd674591bd1334ecab7c1bace
frey-perez <160774979+frey-perez@users.noreply.github.com>
2024-09-13 15:25:40 -0400

    Adjust readers after moving sample data.

--- tests/unit/test_readers.py
+++ tests/unit/test_readers.py
@@ -10,14 +10,16 @@ from wristpy.io.readers import readers
 
 def test_read_invalid_extenstion() -> None:
     """Test the read_watch_data function with an invalid file extension."""
-    path_name = pathlib.Path(__file__).parent / "sample_data" / "example_.txt"
+    path_name = pathlib.Path(__file__).parent.parent / "sample_data" / "example_.txt"
     with pytest.raises(IOError):
         readers.read_watch_data(path_name)
 
 
 def test_gt3x_loader() -> None:
     """Test the gt3x loader."""
-    test_file = pathlib.Path(__file__).parent / "sample_data" / "example_actigraph.gt3x"
+    test_file = (
+        pathlib.Path(__file__).parent.parent / "sample_data" / "example_actigraph.gt3x"
+    )
     watch_data = readers.read_watch_data(test_file)
     assert isinstance(watch_data, models.WatchData)
     assert isinstance(watch_data.acceleration, models.Measurement)
@@ -29,7 +31,9 @@ def test_gt3x_loader() -> None:
 
 def test_geneactiv_bin_loader() -> None:
     """Test the geneActiv bin loader."""
-    test_file = pathlib.Path(__file__).parent / "sample_data" / "example_geneactiv.bin"
+    test_file = (
+        pathlib.Path(__file__).parent.parent / "sample_data" / "example_geneactiv.bin"
+    )
     watch_data = readers.read_watch_data(test_file)
     assert isinstance(watch_data, models.WatchData)
     assert isinstance(watch_data.acceleration, models.Measurement)
