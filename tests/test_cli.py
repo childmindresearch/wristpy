43b3a27ed6dcf2a8a4c98c876b868b5d853dfaff
frey-perez <160774979+frey-perez@users.noreply.github.com>
2024-09-17 10:13:36 -0400

    testing

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,30 +13,48 @@ def test_parse_arguments() -> None:
 
     assert args.input == pathlib.Path("/path/to/input/file.bin")
     assert args.output is None
-    assert isinstance(args.light_threshold, float)
-    assert isinstance(args.moderate_threshold, float)
-    assert isinstance(args.vigorous_threshold, float)
-    assert isinstance(args.chunked, bool)
-    assert isinstance(args.min_acceleration, float)
-    assert isinstance(args.min_hours, int)
-    assert isinstance(args.min_std, float)
-    assert isinstance(args.max_iterations, int)
-    assert isinstance(args.error_tolerance, float)
-    assert isinstance(args.min_calibration_error, float)
-    assert isinstance(args.short_length, int)
-    assert isinstance(args.short_in_long, int)
-    assert isinstance(args.std, float)
-    assert isinstance(args.range, float)
+    assert args.calibrator is None
+    assert isinstance(args.thresholds, float)
 
 
 def test_parse_arguments_with_options() -> None:
     """Test running the argparser with an optional arg."""
     args = cli.parse_arguments(
-        ["/path/to/input/file.bin", "-o", "/path/to/output/file.csv"]
+        [
+            "/path/to/input/file.bin",
+            "-o",
+            "/path/to/output/file.csv",
+            "-c",
+            "ggir",
+            "-t",
+            "0.1",
+            "1.0",
+            "1.5",
+        ]
     )
 
     assert args.input == pathlib.Path("/path/to/input/file.bin")
     assert args.output == pathlib.Path("/path/to/output/file.csv")
+    assert args.calibrator == "ggir"
+    assert args.thresholds == [0.1, 1.0, 1.5]
+
+
+def test_parse_arguments_bad_thresholds() -> None:
+    """Test running the argparser with non ascending thresholds."""
+    with pytest.raises(
+        ValueError, match="Physical activity thresholds must be in ascending order"
+    ):
+        cli.parse_arguments(
+            [
+                "/path/to/input/file.bin",
+                "-o",
+                "/path/to/output/file.csv",
+                "-t",
+                "3.0",
+                "1.0",
+                "1.5",
+            ]
+        )
 
 
 def test_parse_arguements_no_input() -> None:
