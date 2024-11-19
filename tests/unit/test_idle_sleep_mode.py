d2aab2e3f17f817639006ee15bc25df030ec8eef
Adam Santorelli <148909356+Asanto32@users.noreply.github.com>
2024-11-19 11:39:23 -0500

    Update test_idle_sleep_mode.py
    Adding more unit tests

--- tests/unit/test_idle_sleep_mode.py
+++ tests/unit/test_idle_sleep_mode.py
@@ -13,7 +13,9 @@ from wristpy.processing import idle_sleep_mode_imputation
 @pytest.mark.parametrize(
     "sampling_rate, effective_sampling_rate", [(30, 25), (20, 20), (1, 1)]
 )
-def test_idle_sleep_mode(sampling_rate: int, effective_sampling_rate: int) -> None:
+def test_idle_sleep_mode_resampling(
+    sampling_rate: int, effective_sampling_rate: int
+) -> None:
     """Test the idle_sleep_mode function."""
     num_samples = 10000
     dummy_date = datetime(2024, 5, 2)
@@ -31,3 +33,28 @@ def test_idle_sleep_mode(sampling_rate: int, effective_sampling_rate: int) -> No
     )
 
     assert filled_acceleration.time.diff().mean() == 1e9 / effective_sampling_rate
+
+
+def test_idle_sleep_mode_gap_fill() -> None:
+    """Test the idle_sleep_mode gap fill functionality."""
+    num_samples = 10000
+    dummy_date = datetime(2024, 5, 2)
+    dummy_datetime_list = [
+        dummy_date + timedelta(seconds=i) for i in range(num_samples // 2)
+    ]
+
+    time_gap = dummy_date + timedelta(seconds=(1000))
+    dummy_datetime_list += [
+        time_gap + timedelta(seconds=i) for i in range(num_samples // 2, num_samples)
+    ]
+    test_time = pl.Series("time", dummy_datetime_list, dtype=pl.Datetime("ns"))
+
+    acceleration = models.Measurement(
+        measurements=np.ones((num_samples, 3)), time=test_time
+    )
+
+    filled_acceleration = idle_sleep_mode_imputation.impute_idle_sleep_mode_gaps(
+        acceleration
+    )
+    assert len(filled_acceleration.measurements) > len(acceleration.measurements)
+    assert filled_acceleration.time.diff().mean() == 1e9
