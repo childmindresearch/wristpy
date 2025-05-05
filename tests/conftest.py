c3ca41eb155de7abe45f505e740a0ff2b99b1c10
frey-perez <160774979+frey-perez@users.noreply.github.com>
2025-05-05 15:02:45 -0400

    Mims assembled (#192)
    * independent mims module.
    
    * combined_mims
    
    * sample_data
    
    * overall test
    
    * paramertrize
    
    * documentation
    
    * note on extra timestamp
    
    * spacing.
    
    * documentation
    
    * test error
    
    * Update src/wristpy/processing/mims.py
    
    Co-authored-by: Adam Santorelli <148909356+Asanto32@users.noreply.github.com>
    
    * seperate mims testing.
    
    * moving fixtures, reorganized testing.
    
    * epochs are floats, truncation moved, new truncation argument.
    
    * doc change.
    
    * doc string, type hinting.
    
    * typing error.
    
    * updated markdown.
    
    * markdown capitalization.
    
    * doc string
    
    ---------
    
    Co-authored-by: Adam Santorelli <148909356+Asanto32@users.noreply.github.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,10 @@
 """Fixtures used by pytest."""
 
 import pathlib
+from datetime import datetime, timedelta
 
+import numpy as np
+import polars as pl
 import pytest
 
 
@@ -57,3 +60,26 @@ def butter_r_version() -> pathlib.Path:
         / "sample_data"
         / "mims_butterworth_filtered_data.csv"
     )
+
+
+@pytest.fixture
+def mims_r_version() -> pathlib.Path:
+    """The actigraphy sample data processed by the mims function in R."""
+    return pathlib.Path(__file__).parent / "sample_data" / "mims_example_full_run.csv"
+
+
+@pytest.fixture
+def create_acceleration() -> pl.DataFrame:
+    """Fixture to create a dummy acceleration DataFrame to be used in multiple tests."""
+    dummy_date = datetime(2024, 5, 2)
+    dummy_datetime_list = [dummy_date + timedelta(seconds=i) for i in range(1000)]
+    test_time = pl.Series("time", dummy_datetime_list)
+    acceleration_polars_df = pl.DataFrame(
+        {
+            "X": np.ones(1000),
+            "Y": np.ones(1000),
+            "Z": np.ones(1000),
+            "time": test_time,
+        }
+    )
+    return acceleration_polars_df
