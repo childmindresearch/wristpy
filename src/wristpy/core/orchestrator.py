89db955affdcf17895248dd8ae77c57e62a165ac
frey-perez <160774979+frey-perez@users.noreply.github.com>
2025-08-01 17:38:17 -0400

    Feature/issue 166/multiple metrics (#233)
    * orchestrator input changes
    
    * loop over metric
    
    * names to function calls.
    
    * multipule metrics, pal
    
    * multiple metrics, pal working fix for orchestrator
    
    * cli fix
    
    * revert bug check change
    
    * threshold change
    
    * threshold parsing.
    
    * cli str fix.
    
    * working tests
    
    * documentation.
    
    * post-merge fixes.
    
    * Json error
    
    * ruff error
    
    * reverting old test change.
    
    * typer error
    
    * mypy errors
    
    * test for typer error.
    
    * removing validation.
    
    * removing redundant test causing error.
    
    * multiple metrics tests for cli and orchestrator.
    
    * extra test for missmatch error.
    
    * review changes.
    
    * mypy error demands tuple unpacking

--- src/wristpy/core/orchestrator.py
+++ src/wristpy/core/orchestrator.py
@@ -20,17 +20,24 @@ logger = config.get_logger()
 
 VALID_FILE_TYPES = (".csv", ".parquet")
 
+DEFAULT_ACTIVITY_THRESHOLDS = {
+    "enmo": (0.0563, 0.1916, 0.6958),
+    "mad": (0.029, 0.338, 0.604),
+    "ag_count": (100, 3000, 5200),
+    "mims": (10.558, 15.047, 19.614),
+}
+
 
 def run(
     input: Union[pathlib.Path, str],
     output: Optional[Union[pathlib.Path, str]] = None,
-    thresholds: Optional[Tuple[float, float, float]] = None,
+    thresholds: Optional[Sequence[Tuple[float, float, float]]] = None,
     calibrator: Union[
         None,
         Literal["ggir", "gradient"],
     ] = "gradient",
     epoch_length: float = 5,
-    activity_metric: Literal["enmo", "mad", "ag_count", "mims"] = "enmo",
+    activity_metric: Sequence[Literal["enmo", "mad", "ag_count", "mims"]] = ["enmo"],
     nonwear_algorithm: Sequence[Literal["ggir", "cta", "detach"]] = ["ggir"],
     verbosity: int = logging.WARNING,
     output_filetype: Literal[".csv", ".parquet"] = ".csv",
@@ -44,19 +51,22 @@ def run(
     Output file names will be derived from original file names in the case of directory
     processing.
 
-
     Args:
         input: Path to the input file or directory of files to be read. Currently,
             this supports .bin and .gt3x
         output: Path to directory data will be saved to. If processing a single file the
             path should end in the save file name in either .csv or .parquet formats.
         thresholds: The cut points for the light, moderate, and vigorous thresholds,
-            given in that order. Values must be asscending, unique, and greater than 0.
-            Default values are optimized for subjects ages 7-11 [1][3].
+            given in that order. One threshold tuple must be provided for each activity
+            metric, in the same order the metrics were specified. To use default values
+            for all metrics, leave thresholds as None. Values must be ascending, unique,
+            and greater than 0. Default values are optimized for subjects ages 7-11 [1]
+            [3].
         calibrator: The calibrator to be used on the input data.
         epoch_length: The temporal resolution in seconds, the data will be down sampled
             to. It must be > 0.0.
-        activity_metric: The metric to be used for physical activity categorization.
+        activity_metric: The metric(s) to be used for physical activity categorization.
+            Multiple metrics can be specified as a sequence.
         nonwear_algorithm: The algorithm to be used for nonwear detection.
         verbosity: The logging level for the logger.
         output_filetype: Specifies the data format for the save files. Only used when
@@ -67,9 +77,10 @@ def run(
         dictionary of OrchestratorResults objects.
 
     Raises:
+        ValueError: If the number of physical activity thresholds does not match the
+            number of provided activity metrics.
         ValueError: If the physical activity thresholds are not unique or not in
             ascending order.
-
 
     References:
         [1] Hildebrand, M., et al. (2014). Age group comparability of raw accelerometer
@@ -89,19 +100,26 @@ def run(
     input = pathlib.Path(input)
     output = pathlib.Path(output) if output is not None else None
 
-    if activity_metric == "enmo":
-        thresholds = thresholds or (0.0563, 0.1916, 0.6958)
-    elif activity_metric == "mad":
-        thresholds = thresholds or (0.029, 0.338, 0.604)
-    elif activity_metric == "ag_count":
-        thresholds = thresholds or (100, 3000, 5200)
-    elif activity_metric == "mims":
-        thresholds = thresholds or (10.558, 15.047, 19.614)
+    if thresholds is not None:
+        if len(activity_metric) != len(thresholds):
+            raise ValueError(
+                "Number of thresholds did not match the number of activity metrics. "
+                "Provide one threshold tuple per metric or use None for defaults."
+            )
+        metrics_dict = dict(zip(activity_metric, thresholds))
+    else:
+        metrics_dict = {
+            metric: DEFAULT_ACTIVITY_THRESHOLDS[metric] for metric in activity_metric
+        }
 
-    if not (0 <= thresholds[0] < thresholds[1] < thresholds[2]):  # type: ignore
-        message = "Threshold values must be >=0, unique, and in ascending order."
-        logger.error(message)
-        raise ValueError(message)
+    for metric, thresh in metrics_dict.items():
+        if not (0 <= thresh[0] < thresh[1] < thresh[2]):
+            message = (
+                f"Invalid thresholds for {metric}."
+                f" Threshold values must be >=0, unique, and in ascending order."
+            )
+            logger.error(message)
+            raise ValueError(message)
 
     if input.is_file():
         return _run_file(
@@ -131,7 +149,7 @@ def run(
 def _run_directory(
     input: pathlib.Path,
     output: Optional[pathlib.Path] = None,
-    thresholds: Tuple[float, float, float] = (0.0563, 0.1916, 0.6958),
+    thresholds: Optional[Sequence[Tuple[float, float, float]]] = None,
     calibrator: Union[
         None,
         Literal["ggir", "gradient"],
@@ -140,39 +158,41 @@ def _run_directory(
     nonwear_algorithm: Sequence[Literal["ggir", "cta", "detach"]] = ["ggir"],
     verbosity: int = logging.WARNING,
     output_filetype: Literal[".csv", ".parquet"] = ".csv",
-    activity_metric: Literal["enmo", "mad", "ag_count", "mims"] = "enmo",
+    activity_metric: Sequence[Literal["enmo", "mad", "ag_count", "mims"]] = ["enmo"],
 ) -> Dict[str, writers.OrchestratorResults]:
-    """Runs main processing steps for wristpy on  directories.
+    """Runs main processing steps for wristpy on directories.
 
-    The run_directory() function will execute the run_file() function on entire
-    directories. The input and output (if any) paths must directories. Output file
+    The _run_directory() function will execute the _run_file() function on entire
+    directories. The input and output (if any) paths must be directories. Output file
     names will be derived from input file names.
-
 
     Args:
         input: Path to the input directory of files to be read. Currently,
             this supports .bin and .gt3x
         output: Path to directory data will be saved to.
         thresholds: The cut points for the light, moderate, and vigorous thresholds,
-            given in that order. Values must be asscending, unique, and greater than 0.
-            Default values are optimized for subjects ages 7-11 [1][2].
+            given in that order. One threshold tuple must be provided for each activity
+            metric, in the same order the metrics were specified. To use default values
+            for all metrics, leave thresholds as None. Values must be ascending, unique,
+            and greater than 0. Default values are optimized for subjects ages 7-11
+            [1][2].
         calibrator: The calibrator to be used on the input data.
         epoch_length: The temporal resolution in seconds, the data will be down sampled
             to. It must be > 0.0.
         nonwear_algorithm: The algorithm to be used for nonwear detection.
         verbosity: The logging level for the logger.
         output_filetype: Specifies the data format for the save files.
-        activity_metric: The metric to be used for physical activity categorization.
+        activity_metric: The metric(s) to be used for physical activity categorization.
+            Multiple metrics can be specified as a sequence.
 
     Returns:
         All calculated data in a save ready format as a dictionary of
         OrchestratorResults objects.
 
     Raises:
-        ValueError: The output given is not a directory.
-        ValueError: The output_filetype is not a valid type.
+        ValueError: If the output given is not a directory.
+        ValueError: If the output_filetype is not a valid type.
         FileNotFoundError: If the input directory contained no files of a valid type.
-
 
     References:
         [1] Hildebrand, M., et al. (2014). Age group comparability of raw accelerometer
@@ -200,7 +220,6 @@ def _run_directory(
         raise exceptions.EmptyDirectoryError(
             f"Directory {input} contains no .gt3x or .bin files."
         )
-
     results_dict = {}
     for file in file_names:
         output_file_path = (
@@ -234,23 +253,23 @@ def _run_directory(
 def _run_file(
     input: pathlib.Path,
     output: Optional[pathlib.Path] = None,
-    thresholds: Tuple[float, float, float] = (0.0563, 0.1916, 0.6958),
+    thresholds: Optional[Sequence[Tuple[float, float, float]]] = None,
     calibrator: Union[
         None,
         Literal["ggir", "gradient"],
     ] = "gradient",
     epoch_length: float = 5.0,
-    activity_metric: Literal["enmo", "mad", "ag_count", "mims"] = "enmo",
+    activity_metric: Sequence[Literal["enmo", "mad", "ag_count", "mims"]] = ["enmo"],
     nonwear_algorithm: Sequence[Literal["ggir", "cta", "detach"]] = ["ggir"],
     verbosity: int = logging.WARNING,
 ) -> writers.OrchestratorResults:
     """Runs main processing steps for wristpy and returns data for analysis.
 
     The run_file() function will provide the user with the specified physical activity
-    metric, anglez, physical activity levels, detected sleep periods, and nonwear data.
-    All measures will be in the same temporal resolution.
-    Users may choose from 'ggir' and 'gradient' calibration methods,
-    or enter None to proceed without calibration.
+    metric(s), anglez, physical activity levels, detected sleep periods, and nonwear
+    data. All measures will be in the same temporal resolution. Users may choose from
+    'ggir' and 'gradient' calibration methods, or enter None to proceed without
+    calibration.
 
     Args:
         input: Path to the input file to be read. Currently, this supports .bin and
@@ -258,12 +277,16 @@ def _run_file(
         output: Path to save data to. The path should end in the save file name in
             either .csv or .parquet formats.
         thresholds: The cut points for the light, moderate, and vigorous thresholds,
-            given in that order. Values must be ascending, unique, and greater than 0.
-            Default values are optimized for subjects ages 7-11 [1] - [3].
+            given in that order. One threshold tuple must be provided for each activity
+            metric, in the same order the metrics were specified. To use default values
+            for all metrics, leave thresholds as None. Values must be ascending, unique,
+            and greater than 0. Default values are optimized for subjects ages 7-11
+            [1]-[3].
         calibrator: The calibrator to be used on the input data.
         epoch_length: The temporal resolution in seconds, the data will be down sampled
             to. It must be > 0.0.
-        activity_metric: The metric to be used for physical activity categorization.
+        activity_metric: The metric(s) to be used for physical activity categorization.
+            Multiple metrics can be specified as a sequence.
         nonwear_algorithm: The algorithm to be used for nonwear detection. A sequence of
             algorithms can be provided. If so, a majority vote will be taken.
         verbosity: The logging level for the logger.
@@ -272,6 +295,10 @@ def _run_file(
         All calculated data in a save ready format as a OrchestratorResults object.
 
     Raises:
+        ValueError: If the number of physical activity thresholds does not match the
+            number of provided activity metrics.
+        ValueError: If the physical activity thresholds are not unique or not in
+            ascending order.
         ValueError: If an invalid Calibrator is chosen.
         ValueError: If epoch_length is not greater than 0.
 
@@ -326,12 +353,34 @@ def _run_file(
     anglez = metrics.angle_relative_to_horizontal(
         calibrated_acceleration, epoch_length=epoch_length
     )
-    activity_measurement = _compute_activity(
-        calibrated_acceleration,
-        activity_metric,
-        epoch_length,
-        dynamic_range=watch_data.dynamic_range,
-    )
+
+    metrics_dict: Dict[
+        Literal["enmo", "mad", "ag_count", "mims"], Tuple[float, float, float]
+    ]
+    if thresholds is not None:
+        metrics_dict = dict(zip(activity_metric, thresholds))
+    else:
+        metrics_dict = {
+            metric: DEFAULT_ACTIVITY_THRESHOLDS[metric] for metric in activity_metric
+        }
+
+    activity_measurements_list = []
+    physical_activity_levels_list = []
+    for metric_name, thresh in metrics_dict.items():
+        metric_measurement = _compute_activity(
+            calibrated_acceleration,
+            metric_name,
+            epoch_length,
+            dynamic_range=watch_data.dynamic_range,
+        )
+        activity_measurements_list.append(metric_measurement)
+        physical_activity_levels_list.append(
+            analytics.compute_physical_activty_categories(
+                metric_measurement,
+                thresh,
+                name=f"{metric_name} physical activity levels",
+            )
+        )
 
     nonwear_array = processing_utils.get_nonwear_measurements(
         calibrated_acceleration=calibrated_acceleration,
@@ -341,12 +390,8 @@ def _run_file(
 
     nonwear_epoch = processing_utils.synchronize_measurements(
         data_measurement=nonwear_array,
-        reference_measurement=activity_measurement,
+        reference_measurement=anglez,
         epoch_length=epoch_length,
-    )
-
-    physical_activity_levels = analytics.compute_physical_activty_categories(
-        activity_measurement, thresholds
     )
 
     sleep_detector = analytics.GgirSleepDetection(anglez)
@@ -357,18 +402,18 @@ def _run_file(
     spt_windows = analytics.sleep_bouts_cleanup(
         sleep_parameter=sleep_parameters.spt_windows,
         nonwear_measurement=nonwear_epoch,
-        time_reference_measurement=activity_measurement,
+        time_reference_measurement=anglez,
         epoch_length=epoch_length,
     )
     sib_periods = analytics.sleep_bouts_cleanup(
         sleep_parameter=sleep_parameters.sib_periods,
         nonwear_measurement=nonwear_epoch,
-        time_reference_measurement=activity_measurement,
+        time_reference_measurement=anglez,
         epoch_length=epoch_length,
     )
 
     parameters_dictionary = {
-        "thresholds": list(thresholds),
+        "thresholds": list(thresholds) if thresholds is not None else None,
         "calibrator": calibrator,
         "epoch_length": epoch_length,
         "activity_metric": activity_metric,
@@ -377,9 +422,9 @@ def _run_file(
     }
 
     results = writers.OrchestratorResults(
-        physical_activity_metric=activity_measurement,
+        physical_activity_metric=activity_measurements_list,
         anglez=anglez,
-        physical_activity_levels=physical_activity_levels,
+        physical_activity_levels=physical_activity_levels_list,
         sleep_status=sleep_array,
         sib_periods=sib_periods,
         spt_periods=spt_windows,
@@ -433,19 +478,24 @@ def _compute_activity(
     """
     if activity_metric == "ag_count":
         return metrics.actigraph_activity_counts(
-            acceleration,
-            epoch_length=epoch_length,
+            acceleration, epoch_length=epoch_length, name=activity_metric
         )
     elif activity_metric == "mad":
-        return metrics.mean_amplitude_deviation(acceleration, epoch_length=epoch_length)
+        return metrics.mean_amplitude_deviation(
+            acceleration, epoch_length=epoch_length, name=activity_metric
+        )
     elif activity_metric == "mims":
         if dynamic_range is None:
             return metrics.monitor_independent_movement_summary_units(
-                acceleration,
-                epoch=epoch_length,
+                acceleration, epoch=epoch_length, name=activity_metric
             )
         return metrics.monitor_independent_movement_summary_units(
-            acceleration, epoch=epoch_length, dynamic_range=dynamic_range
+            acceleration,
+            epoch=epoch_length,
+            dynamic_range=dynamic_range,
+            name=activity_metric,
         )
 
-    return metrics.euclidean_norm_minus_one(acceleration, epoch_length=epoch_length)
+    return metrics.euclidean_norm_minus_one(
+        acceleration, epoch_length=epoch_length, name=activity_metric
+    )
